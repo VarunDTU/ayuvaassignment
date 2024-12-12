6a47cb22f735022b2084db69c4087c3f6e7d5ebb
Varun Negi <varun.py10@gmail.com>
2024-12-12 14:35:27 +0530

    Implement card component with hover effects and add dependencies for motion and icons

--- app/page.js
+++ app/page.js
@@ -1,101 +1,60 @@
-import Image from "next/image";
+"use client";
+import { useState } from "react";
+import { FaArrowAltCircleRight } from "react-icons/fa";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [active, setActive] = useState(1);
+  const cards = [
+    {
+      title: "Strength",
+      description: "This is card 1",
+      img: "https://images.pexels.com/photos/1458696/pexels-photo-1458696.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+    {
+      title: "Mobility",
+      description: "This is card 2",
+      img: "https://images.pexels.com/photos/15539380/pexels-photo-15539380/free-photo-of-a-great-white-shark-swimming-in-the-ocean.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+    {
+      title: "Drills",
+      description: "This is card 3",
+      img: "https://images.pexels.com/photos/9517922/pexels-photo-9517922.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+  ];
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+  return (
+    <div className="w-screen h-screen flex items-center justify-center">
+      <div className="w-3/4 h-1/2  rounded-s-[50px] rounded-e-[50px] flex flex-row justify-between overflow-hidden  ">
+        {cards.map((val, index) => {
+          return (
+            <div
+              key={index}
+              className={`${
+                active == index ? "w-3/4" : "w-1/4"
+              }  transform duration-200 relative ease-in `}
+              onClick={() => setActive(index)}
+              onMouseEnter={() => setActive(index)}
+              onMouseLeave={() => setActive(1)}
+            >
+              <img
+                src={val.img}
+                className="w-full h-full object-cover -z-50 "
+              />
+              <div className="absolute text-xl uppercase text-white top-1 m-4  mt-4 font-bold ">
+                {val.title}
+              </div>
+
+              <div
+                className={`${
+                  active == index ? "absolute" : "hidden"
+                } text-xl uppercase text-white right-1 top-1 m-4  mt-4 font-bold `}
+              >
+                <FaArrowAltCircleRight />
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
